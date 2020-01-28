be4ca6345585190e31b1f7139894f63335ef76e6
gerardo martinez <gerardo.martinez@tectijuana.edu.mx>
2020-01-27 23:52:10 -0800

    upload proyect test

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirecTo: '/alert'
+    },
+    {
+      icon: 'beaker',
+      name: 'Avatar',
+      redirecTo: '/avatar'
+    },
+    {
+      icon: 'radio-button-on',
+      name: 'Botones y router',
+      redirecTo: '/botones'
+    },
+    {
+      icon: 'card',
+      name: 'Cards',
+      redirecTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checkbox',
+      redirecTo: '/check'
+    },
+    {
+      icon: 'calendar',
+      name: 'DateTime',
+      redirecTo: '/data-time'
+    },
+    {
+      icon: 'car',
+      name: 'Fabs',
+      redirecTo: '/fab'
+    },
+    {
+      icon: 'grid',
+      name: 'Grid - Rows',
+      redirecTo: '/grid'
+    },
+    {
+      icon: 'infinite',
+      name: 'Infinite Scroll',
+      redirecTo: '/infinite-scroll'
+    },
+    {
+      icon: 'hammer',
+      name: 'Input - Forms',
+      redirecTo: '/input'
+    },
+    {
+      icon: 'list',
+      name: 'Listas - Sliding',
+      redirecTo: '/list'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
