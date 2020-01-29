4a88e3e338ba3ab0dd3e48e79e68395641e22db4
gerardo martinez <gerardo.martinez@tectijuana.edu.mx>
2020-01-29 11:07:10 -0800

    Add new metod list

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -8,6 +9,8 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+//Documentacion update
+  @ViewChild('lista', {static: false}) lista: IonList;
 
   usuarios: Observable<any>;
 
@@ -18,5 +21,22 @@ export class ListPage implements OnInit {
     this.usuarios = this.dataService.getUsers();
 
   }
+
+    favorite(user) {
+      console.log('favorite', user);
+      this.lista.closeSlidingItems();
+    }
+
+    share(user) {
+      console.log('share', user);
+      this.lista.closeSlidingItems();
+    }
+
+    borrar(user) {
+      console.log('borrar', user);
+      this.lista.closeSlidingItems();
+    }
+
+
 
 }
